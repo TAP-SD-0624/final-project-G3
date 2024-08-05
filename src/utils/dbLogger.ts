1738cb1d8d187a23f72026ee272ae63e91179ed1
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-05 15:26:39 +0300

    install and use the colorette module for the DB logger

---
+++ src/utils/dbLogger.ts
@@ -0,0 +1,8 @@
+import { magenta } from "colorette";
+
+const dbLogger = (message: string) => {
+  // to colorize the logs of the database to differentiate them from the rest of the logs
+  console.log(magenta(`[Sequelize]: ${message}`));
+};
+
+export default dbLogger;
