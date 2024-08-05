62b4564669ccac99e984c6ad9e30f53a2c9f9703
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-05 15:31:11 +0300

    refactor the server.ts file and sync the DB

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
-import dotenv from 'dotenv';
-import app from './app';
+import dotenv from "dotenv";
+import app from "./app";
+import sequelize from "./database";
 
-dotenv.config(); // to read environment variables from .env file
+dotenv.config();
 
-const PORT: string | undefined = process.env.PORT;
+const PORT: number | undefined = Number(process.env.PORT) || 80;
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+// Sync database and start server
+const startServer = async () => {
+  try {
+    await sequelize.sync(); // { force: true } for development only to drop and recreate tables
+    console.log("Database & tables created!");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to start the server:", error);
+  }
+};
+
+startServer();
