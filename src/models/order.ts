997a6d666712b5702c9dd603e0b5ac5f802c6cf4
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-06 15:03:55 +0300

    create the order model

---
+++ src/models/order.ts
@@ -0,0 +1,48 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../database";
+import { OrderStatus } from "../enums/orderStatus";
+
+class Order extends Model {
+  public id!: string;
+  public totalAmount!: number;
+  public orderStatus!: OrderStatus;
+}
+
+Order.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    totalAmount: {
+      type: DataTypes.DOUBLE,
+      allowNull: false,
+    },
+    orderStatus: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [
+            [
+              OrderStatus.Completed,
+              OrderStatus.Processing,
+              OrderStatus.Canceled,
+            ],
+          ],
+          msg: "Invalid order status",
+        },
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: "Order",
+    tableName: "order",
+    timestamps: true,
+    updatedAt: false,
+  }
+);
+
+export default Order;
