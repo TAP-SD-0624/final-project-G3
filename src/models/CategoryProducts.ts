1a38e9c723b8dac0f5a31e9220c9fe7f65df956a
Rawan M Al-Shurbaji <rawnsh2218@gmail.com>
2024-08-07 06:53:42 +0300

    first commit

--- src/models/CategoryProducts.ts
+++ src/models/CategoryProducts.ts
@@ -1,28 +1,35 @@
 // CategoryProduct.ts
 import { DataTypes, Model } from "sequelize";
-import sequelize from "../database"; 
+import sequelize from "../database";
 
 class CategoryProduct extends Model {
+  public id!: string;
   public productID!: string;
   public categoryID!: string;
 }
 
 CategoryProduct.init(
   {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
     productID: {
       type: DataTypes.UUID,
       allowNull: false,
       references: {
-        model: 'products',
-        key: 'id',
+        model: "products",
+        key: "id",
       },
     },
     categoryID: {
       type: DataTypes.UUID,
       allowNull: false,
       references: {
-        model: 'categories',
-        key: 'id',
+        model: "categories",
+        key: "id",
       },
     },
   },
