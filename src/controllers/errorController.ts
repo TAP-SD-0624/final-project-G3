9bd9c276253dd876d0bc0491eaf077d4eab2c532
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-08 22:48:59 +0300

    fix eslint issues

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,16 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
-import APIError from '../utils/APIError';
+import { Request, Response } from "express";
+import APIError from "../utils/APIError";
 
-const errorController = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof APIError)
-        return res.status(err.statusCode).json({
-            status: 'fail',
-            message: err.message
+const errorController = (err: Error, req: Request, res: Response) => {
+  if (err instanceof APIError)
+    return res.status(err.statusCode).json({
+      status: "fail",
+      message: err.message,
     });
-    res.status(500).json({
-        status: 'error',
-        message: err.message
-    });
-}
+  res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
+};
 
 export default errorController;
