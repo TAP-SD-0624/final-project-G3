b62b45b653993ba0ea6a7093eda932c0f9a4a3fd
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-08 23:52:16 +0300

    fix linting roles

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,19 +3,19 @@ import errorHandler from '../utils/errorHandler';
 import APIError from '../utils/APIError';
 
 const signup = errorHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async(req: Request, res: Response, next: NextFunction) => {
     return next(new APIError('something went wrong', 500));
   },
 );
 
 const login = errorHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async(req: Request, res: Response, next: NextFunction) => {
     return next(new APIError('something went wrong', 500));
   },
 );
 
 const logout = errorHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async(req: Request, res: Response, next: NextFunction) => {
     return next(new APIError('something went wrong', 500));
   },
 );
