bb2711b4293039d3b6fba02b42166492fa7db335
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-08 13:57:10 +0300

    first commit to be fixed

--- src/models/Address.ts
+++ src/models/Address.ts
@@ -4,29 +4,31 @@ import sequelize from "../database";
 class Address extends Model {}
 
 Address.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true
-        },
-        city: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            unique: true
-        },       
-        street: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        },
-        buildingNumber: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        modelName: "Address",
-        tableName: "addresses",
-        timestamps: false
-    }
+    city: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    street: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    buildingNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Address",
+    tableName: "addresses",
+    timestamps: false,
+  }
 );
+
+export default Address;
---
+++ src/models/associations.ts
@@ -0,0 +1,8 @@
+import Address from "./Address";
+import Brand from "./brand";
+import CarouselSlide from "./CarouselSlide";
+import Category from "./category";
+import Order from "./order";
+import OrderItem from "./OrderItem";
+import ProdctImage from "./ProdctImage";
+// import User from "./User";
