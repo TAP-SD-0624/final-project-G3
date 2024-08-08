539abf8499dbbeae7a05671ca8ab5c48f153e79d
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-08 15:38:40 +0300

    wrap the associations with function

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -9,61 +9,64 @@ import User from "./User";
 import WishList from "./Wishlist";
 import Review from "./Review";
 
-// --------------- User Associations --------------- :
+const associateModels = () => {
+  // --------------- User Associations --------------- :
 
-// A user has one order
-User.hasOne(Order, { foreignKey: "userId" });
-Order.belongsTo(User, { foreignKey: "userId" });
+  // A user has one order
+  User.hasOne(Order, { foreignKey: "userId" });
+  Order.belongsTo(User, { foreignKey: "userId" });
 
-// A user has one wishlist which has many products , and wishlist belongs to one user .
-User.hasOne(WishList, { foreignKey: "userId" });
-WishList.belongsTo(User, { foreignKey: "userId" });
+  // A user has one wishlist which has many products , and wishlist belongs to one user .
+  User.hasOne(WishList, { foreignKey: "userId" });
+  WishList.belongsTo(User, { foreignKey: "userId" });
 
-// A user has many reviews but a review belongs to one user
-User.hasMany(Review, { foreignKey: "userId" });
-Review.belongsTo(User, { foreignKey: "userId" });
+  // A user has many reviews but a review belongs to one user
+  User.hasMany(Review, { foreignKey: "userId" });
+  Review.belongsTo(User, { foreignKey: "userId" });
 
-// --------------- Product Associations --------------- :
+  // --------------- Product Associations --------------- :
 
-// A product belongs to one brand while a brand has many products
-Brand.hasMany(Product, { foreignKey: "brandId" });
-Product.belongsTo(Brand, { foreignKey: "brandId" });
+  // A product belongs to one brand while a brand has many products
+  Brand.hasMany(Product, { foreignKey: "brandId" });
+  Product.belongsTo(Brand, { foreignKey: "brandId" });
 
-// A product belongs to one category while a category has many products
-Category.hasMany(Product, { foreignKey: "categoryId" });
-Product.belongsTo(Category, { foreignKey: "categoryId" });
+  // A product belongs to one category while a category has many products
+  Category.hasMany(Product, { foreignKey: "categoryId" });
+  Product.belongsTo(Category, { foreignKey: "categoryId" });
 
-// A product has many images in productImages table and each image belongs to one product
-Product.hasMany(ProductImage, { foreignKey: "productId" });
-ProductImage.belongsTo(Product, { foreignKey: "productId" });
+  // A product has many images in productImages table and each image belongs to one product
+  Product.hasMany(ProductImage, { foreignKey: "productId" });
+  ProductImage.belongsTo(Product, { foreignKey: "productId" });
 
-// An order has many products through order items
-Order.belongsToMany(Product, { through: "orderItems", foreignKey: "orderId" });
-Product.belongsToMany(Order, {
-  through: "orderItems",
-  foreignKey: "productId",
-});
+  // An order has many products through order items
+  Order.belongsToMany(Product, {
+    through: "orderItems",
+    foreignKey: "orderId",
+  });
+  Product.belongsToMany(Order, {
+    through: "orderItems",
+    foreignKey: "productId",
+  });
 
-// A product has many reviews but a review belongs to one product
-Product.hasMany(Review, { foreignKey: "productId" });
-Review.belongsTo(Product, { foreignKey: "productId" });
+  // A product has many reviews but a review belongs to one product
+  Product.hasMany(Review, { foreignKey: "productId" });
+  Review.belongsTo(Product, { foreignKey: "productId" });
 
-// A Wishlist has many products and one product belongs to many wishlists .
-WishList.belongsToMany(Product, { through: "wishListItems" });
-Product.belongsToMany(WishList, { through: "wishListItems" });
+  // A Wishlist has many products and one product belongs to many wishlists .
+  WishList.belongsToMany(Product, { through: "wishListItems" });
+  Product.belongsToMany(WishList, { through: "wishListItems" });
 
-// --------------- Address Associations --------------- :
+  // --------------- Address Associations --------------- :
 
-// An address belongs to one order and an order has one address
-Address.hasOne(Order, { foreignKey: "addressId" });
-Order.belongsTo(Address, { foreignKey: "addressId" });
+  // An address belongs to one order and an order has one address
+  Address.hasOne(Order, { foreignKey: "addressId" });
+  Order.belongsTo(Address, { foreignKey: "addressId" });
 
-// --------------- Carousel Slides Associations --------------- :
+  // --------------- Carousel Slides Associations --------------- :
 
-// A category has one carousel slide and a carousel slide belongs to one category
-Category.hasOne(CarouselSlide, { foreignKey: "categoryId" });
-CarouselSlide.belongsTo(Category, { foreignKey: "categoryId" });
-
-const associateModels = () => {};
+  // A category has one carousel slide and a carousel slide belongs to one category
+  Category.hasOne(CarouselSlide, { foreignKey: "categoryId" });
+  CarouselSlide.belongsTo(Category, { foreignKey: "categoryId" });
+};
 
 export default associateModels;
